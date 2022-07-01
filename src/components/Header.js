468278d95c1e94f597762e5ad5e1c9c7e82762ba
Rohit Parakh <rohitparakh4@gmail.com>
2022-07-01 12:22:42 +0530

    Updated  header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import logo from '../assets/logo.jpg'
 const Header = () => {
   return (
     <div className="header">
         <Link to="/">
-        <div className='logo'>Logo</div>
+        <div className='logo'>
+            <img src={logo}/>
+        </div>
         </Link>
         <Link to="/">
-        <div className='name'>Valorant Rohit</div>
+        <div className='name'>Valorant Stats by Rohit</div>
         </Link>
     </div>
   )
